b07a4a58155d62a1011079cc08005d9f1cad10c1
Beq <beqjanus@gmail.com>
2022-11-21 23:00:53 +0000

    revert older changes

--- scripts/download/macOS.sh
+++ scripts/download/macOS.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -eu
-# curl -s $GITHUB_API_URL/repos/rclone/rclone/releases/latest
-version=$(curl -s $GITHUB_API_URL/repos/rclone/rclone/releases/latest | jq -r .tag_name)
+set -euo pipefail
+version=$(curl -s https://api.github.com/repos/rclone/rclone/releases/latest | jq -r '.tag_name')
 echo ::group::Downloading Rclone $version for macOS
 aria2c -x 16 $GITHUB_SERVER_URL/rclone/rclone/releases/download/$version/rclone-$version-osx-amd64.zip
 7z e rclone-$version-osx-amd64.zip rclone -oRclone -r
