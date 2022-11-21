15068964e73aeac1aeb47dda7f9b63138c8da73f
Beq <beqjanus@gmail.com>
2022-11-21 23:29:01 +0000

    why is this not forcing?

--- scripts/download/macOS.sh
+++ scripts/download/macOS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
-version = "v1.60.1"
-echo ::group::Downloading Rclone $version for macOS
+version="v1.60.1"
+echo ::group::Downloading rclone $version for macOS
 aria2c -x 16 $GITHUB_SERVER_URL/rclone/rclone/releases/download/$version/rclone-$version-osx-amd64.zip
 7z e rclone-$version-osx-amd64.zip rclone -oRclone -r
 rm rclone-$version-osx-amd64.zip
