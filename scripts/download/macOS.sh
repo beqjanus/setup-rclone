1258047797d746c5604c5fdf82a69a9de14d9e15
Beq <beqjanus@gmail.com>
2022-11-21 22:51:02 +0000

    try to work out why this fails

--- scripts/download/macOS.sh
+++ scripts/download/macOS.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
+curl -s $GITHUB_API_URL/repos/rclone/rclone/releases/latest
 version=$(curl -s $GITHUB_API_URL/repos/rclone/rclone/releases/latest | jq -r .tag_name)
 echo ::group::Downloading Rclone $version for macOS
 aria2c -x 16 $GITHUB_SERVER_URL/rclone/rclone/releases/download/$version/rclone-$version-osx-amd64.zip
