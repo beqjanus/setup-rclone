713d88fe496e5504fb35483d5ffce258570f3600
Beq <beqjanus@gmail.com>
2022-11-21 23:16:37 +0000

    try a default value

--- scripts/download/macOS.sh
+++ scripts/download/macOS.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
-version=$(curl -s https://api.github.com/repos/rclone/rclone/releases/latest | jq -r .tag_name)
+version=$($(curl -s https://api.github.com/repos/rclone/rclone/releases/latest | jq -r .tag_name):-"v1.60.1")
+
 echo ::group::Downloading Rclone $version for macOS
 aria2c -x 16 $GITHUB_SERVER_URL/rclone/rclone/releases/download/$version/rclone-$version-osx-amd64.zip
 7z e rclone-$version-osx-amd64.zip rclone -oRclone -r
