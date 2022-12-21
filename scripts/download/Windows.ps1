9b2f9ed123f661c0595ee4122c2c7e6e4cf35044
Beq <beqjanus@gmail.com>
2022-12-21 02:59:25 +0000

    fix bad file name

--- scripts/download/Windows.ps1
+++ scripts/download/Windows.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 Write-Host ::group::Downloading Rclone current for Windows
-aria2c -x 16 https://downloads.rclone.org/rclone-current-osx-amd64.zip
+aria2c -x 16 https://downloads.rclone.org/rclone-current-windows-amd64.zip
 7z e rclone-current-windows-amd64.zip rclone.exe -oRclone -r
 Remove-Item rclone-current-windows-amd64.zip
 Write-Host ::endgroup::
